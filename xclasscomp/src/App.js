0fd5131e056f9d866da09993d4ce0e12d0eb346b
vkzeus <150108950+vkzeus@users.noreply.github.com>
2024-12-27 14:32:34 +0530

    Update App.js

--- xclasscomp/src/App.js
+++ xclasscomp/src/App.js
@@ -4,16 +4,16 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0, // Initialize the count state
+      count: 0, 
     };
   }
 
-  // Method to handle increment
+ 
   handleIncrement = () => {
     this.setState((prevState) => ({ count: prevState.count + 1 }));
   };
 
-  // Method to handle decrement
+
   handleDecrement = () => {
     this.setState((prevState) => ({ count: prevState.count - 1 }));
   };
@@ -21,15 +21,15 @@ class Counter extends Component {
   render() {
     return (
       <div style={{ textAlign: "center", marginTop: "50px" }}>
-        {/* Display the count */}
+    
         <h1>Count: {this.state.count}</h1>
 
-        {/* Increment button */}
+
         <button onClick={this.handleIncrement} style={{ marginRight: "10px" }}>
           Increment
         </button>
 
-        {/* Decrement button */}
+
         <button onClick={this.handleDecrement}>Decrement</button>
       </div>
     );
